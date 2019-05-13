be1851c032883e3bcbcc9984a6d70a9eb2647821
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-05-13 01:08:39 -0500

    avance go zero
    nota defer cola de ejecuciones diferidas

--- ejercicios_go_cero_edt/n6_metodo_interface_panic/interface/pkganimales/gato.go
+++ ejercicios_go_cero_edt/n6_metodo_interface_panic/interface/pkganimales/gato.go
@@ -6,6 +6,6 @@ type Gato struct {
 	Nombre string
 }
 
-func (g Gato) Comunicarse() {
+func (g Gato) Comunicarse() { // los type no pueden ser punteros para implementarse en interface
 	fmt.Println("miauuu")
 }
--- ejercicios_go_cero_edt/n6_metodo_interface_panic/interface/pkganimales/perro.go
+++ ejercicios_go_cero_edt/n6_metodo_interface_panic/interface/pkganimales/perro.go
@@ -6,6 +6,6 @@ type Perro struct {
 	Nombre string
 }
 
-func (p Perro) Comunicarse() { // los type no pueden ser punteross
+func (p Perro) Comunicarse() { // los type no pueden ser punteros para implementarse en interface
 	fmt.Println("wofff")
 }
