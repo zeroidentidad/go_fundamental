549e6bd06697ba6274613f69e3653597568316ac
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-06-02 15:55:57 -0500

    avance go zero
    controlador obtener todos los comentarios y prueba ruta postman method GET

--- ejercicios_go_cero_edt/n9_proyecto_curso/controllers/comment.go
+++ ejercicios_go_cero_edt/n9_proyecto_curso/controllers/comment.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"../commons"
 	"../configuration"
@@ -39,4 +41,56 @@ func CommentCreate(w http.ResponseWriter, r *http.Request) {
 	m.Code = http.StatusCreated
 	m.Message = "Comentario creado"
 	commons.DisplayMessage(w, m)
+}
+
+// CommentGetAll consulta todos los comentarios
+func CommentGetAll(w http.ResponseWriter, r *http.Request) {
+	comments := []models.Comment{}
+	m := models.Message{}
+	user := models.User{}
+	//vote := models.Vote{}
+
+	r.Context().Value(&user)
+	vars := r.URL.Query()
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	cComment := db.Where("parent_id = 0")
+	if order, ok := vars["order"]; ok {
+		if order[0] == "votes" {
+			cComment = cComment.Order("votes desc, created_at desc")
+		}
+	} else {
+		if idLimit, ok := vars["idlimit"]; ok {
+			registerByPage := 30
+			offset, err := strconv.Atoi(idLimit[0])
+			if err != nil {
+				log.Println("Error:", err)
+			}
+			cComment = cComment.Where("id BETWEEN ? AND ?", offset-registerByPage, offset)
+		}
+		cComment = cComment.Order("id desc")
+	}
+
+	//ejecucion de consulta
+	cComment.Find(&comments)
+
+	j, err := json.Marshal(comments)
+	if err != nil {
+		m.Code = http.StatusInternalServerError
+		m.Message = "Error al convertir los comentarios en json"
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	if len(comments) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m.Code = http.StatusNoContent
+		m.Message = "No se encontraron comentrarios"
+		commons.DisplayMessage(w, m)
+	}
 }
--- ejercicios_go_cero_edt/n9_proyecto_curso/routes/comment.go
+++ ejercicios_go_cero_edt/n9_proyecto_curso/routes/comment.go
@@ -11,6 +11,7 @@ func SetCommentRouter(router *mux.Router) {
 	prefix := "/api/comments"
 	subRouter := mux.NewRouter().PathPrefix(prefix).Subrouter().StrictSlash(true)
 	subRouter.HandleFunc("/", controllers.CommentCreate).Methods("POST")
+	subRouter.HandleFunc("/", controllers.CommentGetAll).Methods("GET")
 
 	router.PathPrefix(prefix).Handler(
 		negroni.New(
