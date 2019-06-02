1375e164bb2b84c5d4e861e731fe1299888715bc
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-06-02 11:40:57 -0500

    avance go zero
    version inicial ruta comentario method post

---
+++ ejercicios_go_cero_edt/n9_proyecto_curso/routes/comment.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"../controllers"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+// SetCommentRouter contiene las rutas para la creación y consulta de comentarios
+func SetCommentRouter(router *mux.Router) {
+	prefix := "/api/comments"
+	subRouter := mux.NewRouter().PathPrefix(prefix).Subrouter().StrictSlash(true)
+	subRouter.HandleFunc("/", controllers.CommentCreate).Methods("POST")
+
+	router.PathPrefix(prefix).Handler(
+		negroni.New(
+			negroni.HandlerFunc(controllers.ValidateToken),
+			negroni.Wrap(subRouter),
+		),
+	)
+}
