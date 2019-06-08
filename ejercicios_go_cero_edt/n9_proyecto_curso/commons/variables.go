9f183dcf1c6d6327c82d3d3b77334374a6fe9967
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-06-08 11:52:57 -0500

    refactorizacion ejecutable
    bandera de puerto indicable u opcional

---
+++ ejercicios_go_cero_edt/n9_proyecto_curso/commons/variables.go
@@ -0,0 +1,4 @@
+package commons
+
+// Port es el puerto del servidor web (de los 65536 para uso)
+var Port int
--- ejercicios_go_cero_edt/n9_proyecto_curso/run_app.go
+++ ejercicios_go_cero_edt/n9_proyecto_curso/run_app.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
+	"./commons"
 	"./migration"
 	"./routes"
 	"github.com/urfave/negroni"
 )
 
-// inicio por defecto: ./run_app
-// inicio en blanco: ./run_app --migrate yes
+// inicio por defecto: ./run_app --port 8080
+// inicio en blanco: ./run_app --port 8081 --migrate yes
 func main() {
 	var migrate string
 	flag.StringVar(&migrate, "migrate", "no", "Genera migración a BD")
-	//flag.IntVar(&commons.Port, "port", 8080, "Puerto para servidor web")
+	flag.IntVar(&commons.Port, "port", 8080, "Puerto para servidor web")
 	flag.Parse()
 	if migrate == "yes" {
 		log.Println("Comenzó la migración...")
@@ -32,11 +34,11 @@ func main() {
 
 	// Inicia el servidor
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    fmt.Sprintf(":%d", commons.Port),
 		Handler: n,
 	}
 
-	log.Printf("Iniciado en http://localhost:8080")
+	log.Printf("Iniciado en http://localhost:%d", commons.Port)
 	log.Println(server.ListenAndServe())
 	log.Println("Finalizó la ejecución programa")
 }
