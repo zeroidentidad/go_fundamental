0e427c75ce101c13e6ecfb0153b87c48a0b54fc8
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-07-18 19:54:49 -0500

    template demo
    generacion de  plantilla html basica con datos de archivo de texto como fuente

---
+++ ej_web_template_gen/template_demo/salida_standar/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+)
+
+type curso struct {
+	Numero, Nombre, Unidades string
+}
+
+type semestre struct {
+	Term   string
+	Cursos []curso
+}
+
+func main() {
+	// #1 get datos
+	semestres := academicYear("datos/primer_semestre.txt", "datos/segundo_semestre.txt")
+
+	// #2 parse template
+	tpl, err := template.ParseFiles("template.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// #3 execute template
+	err = tpl.Execute(os.Stdout, semestres)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+func academicYear(s ...string) []semestre {
+	semestres := []semestre{}
+	for _, v := range s {
+		s := semestre{}
+		records := getRecords(v)
+		xc := make([]curso, 0, len(records))
+		// #3 get datos
+		for i, row := range records {
+			if i == 0 {
+				xs := strings.SplitN(row[0], ",", 2)
+				t := xs[0]
+				s.Term = t
+			} else {
+				c := curso{}
+				xs := strings.SplitN(row[0], " ", 2)
+				c.Numero = xs[0]
+				c.Nombre = xs[1]
+				c.Unidades = row[1]
+				xc = append(xc, c)
+			}
+		}
+		s.Cursos = xc
+		semestres = append(semestres, s)
+	}
+	return semestres
+}
+
+func getRecords(path string) [][]string {
+	// #1 open a file
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	// #2 parse a csv file
+	rdr := csv.NewReader(f)
+	rows, err := rdr.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+	return rows
+}
