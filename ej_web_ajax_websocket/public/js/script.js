4a7093104c011c63b5d8ce51f668d4709fe872fe
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-07-25 09:40:59 -0500

    ej web ajax websocket
    revision estados objeto xhr

--- ej_web_ajax_websocket/public/js/script.js
+++ ej_web_ajax_websocket/public/js/script.js
@@ -3,14 +3,19 @@ const cont = document.getElementById('contenido');
 const btn = document.getElementById('btnAjax');
 
 btn.addEventListener('click', e=>{
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET','/data.html', true)
+    const xhr = new XMLHttpRequest();
+    console.log('Estado objeto: ', xhr.readyState);
+
+    xhr.open('GET','/data.html', true);
+    console.log('Estado objeto: ', xhr.readyState);
 
     // como usar lo solicitado
     xhr.addEventListener('load', e =>{
-        cont.innerHTML=e.target.responseText
+        cont.innerHTML=e.target.responseText;
+        console.log('Estado objeto: ', xhr.readyState);
     })
 
     //Peticion
-    xhr.send()
+    xhr.send();
+    console.log('Estado objeto: ', xhr.readyState);
 })
