4afff1f6a1b771c2bd55e28e47e4ac32d312f332
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-07 14:54:43 -0500

    api get con axios
    simplifacion peticion get con axios

---
+++ ej_web_ajax_websocket/js/temaAxios/index.js
@@ -0,0 +1,42 @@
+btn.addEventListener('click', () => {loadComics()});
+
+const loadComics = async()=>{
+    const hash = 'f868e45dbbd38a5d6de3ed86a92f4d4b', apikey = '42de5550980de3333222524000d53b99';
+    const nombre = ipt.value;
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apikey}&hash=${hash}&limit=20&nameStartsWith=${nombre}`;
+
+    const res = await axios.get(url);
+
+    switch (res.status) {
+        case 200:
+            dibujar(res.data.data.results);
+            break;               
+        default:
+            setText(`Error desconocido. Code: ${res.status}`);
+            break;
+    }
+}
+
+const dibujar = data => {
+    while (miContenido.firstChild) {
+        miContenido.removeChild(miContenido.firstChild);
+    }
+
+    const contenedor = document.createElement('div');
+    data.forEach(comic => {
+        const comicHTML = 
+        `<div>
+        <h2>${comic.name}</h2>
+        <img src="${comic.thumbnail.path}/portrait_incredible.${comic.thumbnail.extension}" alt="${comic.name}"/>
+        <p>${comic.description}</p>
+        </div>`
+
+        contenedor.insertAdjacentHTML('beforeend', comicHTML);
+    });
+
+    miContenido.appendChild(contenedor);
+}
+
+const setText = data => {
+    miContenido.innerHTML = data;
+}
---
+++ ej_web_ajax_websocket/js/temaAxios/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	eco := echo.New()
+
+	eco.Use(middleware.Logger())
+	eco.Use(middleware.Recover())
+
+	// CORS config
+	// con metodos permitidos
+	eco.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	eco.Static("/", "./")
+
+	log.Error(eco.Start(":9000"))
+}
