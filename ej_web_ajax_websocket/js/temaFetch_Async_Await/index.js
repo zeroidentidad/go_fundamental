750752f40c7f3d037fe9ab4988964eec39d06efc
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-09 20:28:06 -0500

    api fecth (async await)
    fecth usando async await

--- ej_web_ajax_websocket/js/temaFetch_Async_Await/index.js
+++ ej_web_ajax_websocket/js/temaFetch_Async_Await/index.js
@@ -1,25 +1,21 @@
 btn.addEventListener('click', () => {loadComics()});
 
-const loadComics = ()=>{
+const loadComics = async()=>{
     const hash = 'f868e45dbbd38a5d6de3ed86a92f4d4b', apikey = '42de5550980de3333222524000d53b99';
     const nombre = ipt.value;
     const url = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apikey}&hash=${hash}&limit=20&nameStartsWith=${nombre}`;
 
-    //Fetch
-    fetch(url)
-    .then(res => {
-        switch (res.status) {
-            case 200:
-                return res.json()
-            default:
-                setText(`Error desconocido. Code: ${res.status}`);
+    //Await Fetch
+    const res = await fetch(url)
+    switch (res.status) {
+        case 200:
+            const res_data = await res.json()
+            dibujar(res_data.data.results);
             break;
-        }
-    })
-    .then(res => {
-        dibujar(res.data.results);
-    })
-    .catch(error => setText(`Error: ${error}`));
+        default:
+            setText(`Error desconocido. Code: ${res.status}`);
+            break;
+    }
 }
 
 const dibujar = data => {
