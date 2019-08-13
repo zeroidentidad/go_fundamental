a092eb40f59ea8949bd9c665c86ab4dd2b789485
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-13 11:13:46 -0500

    ej websocket
    codigo interacccion websocket

--- ej_web_ajax_websocket/proyecto_web/public/index.js
+++ ej_web_ajax_websocket/proyecto_web/public/index.js
@@ -17,7 +17,7 @@ const login = async() => {
     switch (response.status) {
         case 200:
            data = await response.json()
-           //connectWS(data)
+            connectWS(data)
             console.log(data)
             setSystemMessage(`Conectado OK. Code: ${response.status}`)
             break;
@@ -33,4 +33,45 @@ const login = async() => {
 btnLogin.addEventListener('click', e => {
     e.preventDefault()
     login()
+})
+
+const connectWS = data => {
+    ws = new WebSocket(`ws://localhost:9000/ws?uname=${usrName.value}&token=${data.token}`)
+    ws.onopen = e => {
+        setSystemMessage('Conectado a websocket correctamente.')
+    }
+    ws.onerror = e => {
+        setSystemMessage(`Ocurrio un error. Desc: ${e}`)
+    }
+    ws.onmessage = e => {
+        const data = JSON.parse(e.data)
+        switch (data.type) {
+            case 'message':
+                content.insertAdjacentHTML('beforeend',`<div>De: ${data.data_response.name}, Mensaje: ${data.data_response.message}</div>`)
+                break;
+            case 'sale':
+
+                break;
+            case 'pong':
+
+                break;                        
+            default:
+                setSystemMessage('Mensaje desconocido recibido')
+                break;
+        }
+    }
+}
+
+btnSend.addEventListener('click', e => {
+    e.preventDefault()
+    let data={};
+    if (e.data === null || e.data ===''){
+        setSystemMessage('Mensaje vacio.')        
+    }else{
+        data = {
+            type: 'message',
+            message: textMsg.value
+        }
+        ws.send(JSON.stringify(data))
+    }
 })
