41c253f3bf0e226cbff3c2ca1822dcb953ec236c
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-08-19 19:57:09 -0500

    ej webchat
    server json

--- ej_webchat_server_sencillo/server.go
+++ ej_webchat_server_sencillo/server.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
+func Hola(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Ke pedo"))
+}
+
+type Response struct {
+	Mensaje string `json:"mensaje"`
+	Nombre  string `json:"nombre"`
+}
+
+func HolaJSON(w http.ResponseWriter, r *http.Request) {
+	resonse := CreateResponse()
+	json.NewEncoder(w).Encode(resonse)
+}
+
+func CreateResponse() Response {
+	return Response{
+		"Ke onda es JSON",
+		"Jesus",
+	}
+}
+
 func main() {
-	log.Println("ke pedo")
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", Hola).Methods("GET")
+	mux.HandleFunc("/json", HolaJSON).Methods("GET")
+
+	http.Handle("/", mux) //se traducen a las rutas de mux
+
+	log.Println("Server ejecutandose")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
