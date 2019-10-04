bc9f7ac7361c888cea97015813a65d856d1cf0db
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-04 13:37:32 -0500

    ej botfb go
    func validacion

--- ej_botfb_go/main.go
+++ ej_botfb_go/main.go
@@ -9,6 +9,8 @@ func main() {
 	getConfig()
 
 	http.HandleFunc("/", saluda)
+	http.HandleFunc("/webhook", webhook)
+
 	//log.Println(http.ListenAndServe("localhost:8080", nil))
 	log.Printf("Serv iniciado https://localhost:%v", c.Port)
 	err := http.ListenAndServeTLS(c.Port, c.CertPem, c.KeyPem, nil)
@@ -19,4 +21,19 @@ func main() {
 
 func saluda(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hola x"))
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		vertoken := r.URL.Query().Get("hub.verify_token")
+		if vertoken == c.MyToken {
+			hubc := r.URL.Query().Get("hub.challenge")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(hubc))
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error en la verificación"))
+		return
+	}
 }
