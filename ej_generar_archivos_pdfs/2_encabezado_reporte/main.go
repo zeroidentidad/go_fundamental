66bf243f7ba89e73a06739efb6ee0c3268a83e6d
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-21 13:40:15 -0500

    ej generar pdf
    encabezado pdf

---
+++ ej_generar_archivos_pdfs/2_encabezado_reporte/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+const (
+	bannerHt = 94.0
+	xIndent  = 40.0
+)
+
+func main() {
+	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
+	w, h := pdf.GetPageSize()
+	fmt.Printf("width=%v, height=%v\n", w, h)
+	pdf.AddPage()
+
+	// Top encabezado con color
+	pdf.SetFillColor(72, 16, 20)
+	pdf.Polygon([]gofpdf.PointType{
+		{0, 0},
+		{w, 0},
+		{w, bannerHt},
+		{0, bannerHt * 0.93}, // menor mañano proporcional
+	}, "F")
+	pdf.Polygon([]gofpdf.PointType{
+		{0, h},
+		{0, h - (bannerHt * 0.2)},
+		{w, h - (bannerHt * 0.1)},
+		{w, h},
+	}, "F")
+
+	//Contenido de prueba para ubicar en posiciones con ayuda de la funcion de cuadricula
+
+	// Banner - REPORTE
+	pdf.SetFont("arial", "B", 40)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt := pdf.GetFontSize()
+	pdf.Text(xIndent, bannerHt-(bannerHt/2.0)+lineHt/3.1, "REPORTE")
+
+	// Banner - telefono, email, website
+	pdf.SetFont("arial", "", 12)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MoveTo(w-xIndent-2.0*124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "(123) 456-7890\ntest@ejemplo.com\nejemplo.com", gofpdf.BorderNone, gofpdf.AlignRight, false)
+
+	// Banner - direccion
+	pdf.SetFont("arial", "", 12)
+	pdf.SetTextColor(255, 255, 255)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MoveTo(w-xIndent-124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "123 Lugar x\nAlguna ciudad, Calle\n12345", gofpdf.BorderNone, gofpdf.AlignRight, false)
+
+	// Texto al centro
+	pdf.SetFont("arial", "", 18)
+	pdf.SetTextColor(0, 0, 0)
+	pdf.Text(214, 300, "Contenido de prueba, de parte de encabezado de pdf")
+
+	// Grid util para las medidas de los elmentos y posicion
+	//drawGrid(pdf)
+	err := pdf.OutputFileAndClose("pf2.pdf")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func drawGrid(pdf *gofpdf.Fpdf) {
+	w, h := pdf.GetPageSize()
+	pdf.SetFont("courier", "", 12)
+	pdf.SetTextColor(80, 80, 80)
+	pdf.SetDrawColor(200, 200, 200)
+	for x := 0.0; x < w; x = x + (w / 20.0) {
+		pdf.SetTextColor(200, 200, 200)
+		pdf.Line(x, 0, x, h)
+		_, lineHt := pdf.GetFontSize()
+		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
+	}
+	for y := 0.0; y < h; y = y + (w / 20.0) {
+		if y < bannerHt*.9 {
+			pdf.SetTextColor(200, 200, 200)
+		} else {
+			pdf.SetTextColor(80, 80, 80)
+		}
+		pdf.Line(0, y, w, y)
+		pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
+	}
+}
