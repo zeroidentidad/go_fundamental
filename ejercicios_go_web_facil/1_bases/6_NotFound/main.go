ca4ff95a4b678b766f17de5cf8c2748103abcc12
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-31 21:49:04 -0600

    go web facil
    not found

---
+++ ejercicios_go_web_facil/1_bases/6_NotFound/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Fprintf(w, "Holas ...")
+		http.Redirect(w, r, "/home", http.StatusMovedPermanently)
+	})
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+
+		http.NotFound(w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// visualizar desde ternimal con: curl -i http://localhost:8080
+// referencia: https://golang.org/src/net/http/status.go
