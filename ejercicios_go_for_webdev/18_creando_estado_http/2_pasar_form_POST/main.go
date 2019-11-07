184b3a78293533075f379a1d0a4ab1ef4f1a729f
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-07 10:13:29 -0600

    go for web
    pasar data en form por post y get

---
+++ ejercicios_go_for_webdev/18_creando_estado_http/2_pasar_form_POST/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", watahea)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func watahea(w http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST">
+	 <input type="text" name="q">
+	 <input type="submit">
+	</form>
+	<br>`+v)
+}
