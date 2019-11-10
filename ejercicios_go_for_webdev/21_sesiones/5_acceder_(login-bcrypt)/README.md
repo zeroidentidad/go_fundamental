49159be36e99382a562e5c61f543bb48896703cf
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-09 18:49:16 -0600

    go for web
    manejo login fake user stored

---
+++ ejercicios_go_for_webdev/21_sesiones/5_acceder_(login-bcrypt)/README.md
@@ -0,0 +1,8 @@
+# Paso 1: 
+Se agrega una pagina ```login.gohtml```.
+
+# Paso 2:
+Manejar el envío de un formulario de inicio de sesión
+
+# Paso 3:
+Se agregó un usuario falso a dbUsers en func init
---
+++ ejercicios_go_for_webdev/21_sesiones/5_acceder_(login-bcrypt)/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type user struct {
+	UserName string
+	Password []byte
+	Nombre   string
+	Apellido string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	dbUsers["test@test.com"] = user{"test@test.com", bs, "Jesus", "Ferrer"}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	var u user
+	// proceso de envío de formulario
+	if req.Method == http.MethodPost {
+		// obtener valores de formulario
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		f := req.FormValue("nombre")
+		l := req.FormValue("apellido")
+		// username en uso?
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+		// crear session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		// almacenar user en dbUsers
+		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		u = user{un, bs, f, l}
+		dbUsers[un] = u
+		// redirect
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "signup.gohtml", u)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// proceso de envío de formulario
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		// hay un username?
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "Nombre de usuario y / o contraseña no coinciden", http.StatusForbidden)
+			return
+		}
+		// la contraseña ingresada coincide con la contraseña almacenada?
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "Nombre de usuario y / o contraseña no coinciden", http.StatusForbidden)
+			return
+		}
+		// crear session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
