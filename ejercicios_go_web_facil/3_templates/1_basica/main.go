7328329ef40bab5eebd0e7b0eafbd35d81f56eae
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-10 20:18:53 -0600

    go web facil
    templates sencillas init

---
+++ ejercicios_go_web_facil/3_templates/1_basica/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	port := "8080"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template, err := template.New("Hola").Parse("Watahea :/")
+
+		if err != nil {
+			panic(err)
+		}
+
+		template.Execute(w, nil)
+	})
+
+	fmt.Println("Listen puerto :", port)
+	http.ListenAndServe(":"+port, nil)
+}
