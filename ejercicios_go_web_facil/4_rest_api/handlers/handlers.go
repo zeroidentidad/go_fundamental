4a4777b2a1fd9cdfcb9fecfb46df284305cf2862
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-14 21:37:48 -0600

    go web facil
    respuesta json

--- ejercicios_go_web_facil/4_rest_api/handlers/handlers.go
+++ ejercicios_go_web_facil/4_rest_api/handlers/handlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"../models"
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +13,10 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Get de un usuario\n")
+	user := models.User{ID: 1, Username: "Zero", Password: "xd"}
+	w.Header().Set("Content-Type", "application/json")
+	userjson, _ := json.Marshal(user)
+	fmt.Fprintf(w, string(userjson))
 }
 
 func PostUser(w http.ResponseWriter, r *http.Request) {
