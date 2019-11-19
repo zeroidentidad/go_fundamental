1533bedc5a1f6edb2ff95a238a46356f14311cbe
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-18 19:34:15 -0600

    go for web
    subir y mostrar imagenes

---
+++ ejercicios_go_for_webdev/23_subir_mostrar_imagenes/3_mostrar_imagenes/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	// agregar ruta para servir fotos
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	// proceso de envío de formulario
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		// crear sha para el nombre del archivo
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		// crear nuevo archivo
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		// copiar
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		// agregar nombre de archivo a la cookie de este usuario
+		c = appendValue(w, c, fname)
+	}
+	xs := strings.Split(c.Value, "|")
+	// valores de cookies en slice para enviar solo parte datos imágenes
+	tpl.ExecuteTemplate(w, "index.gohtml", xs[1:])
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+// toma un nombre de archivo ahora también en la cookie
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+}
