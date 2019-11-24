c1f78a53ba1ec0433452c77b5b8927f9d70dad31
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-23 18:56:52 -0600

    go web facil
    version modificada con paquete config

---
+++ ejercicios_go_web_facil/6_configuracion_env/models/database.go
@@ -0,0 +1,78 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"../config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func CreateConnection() {
+	url := config.UrlDatabase()
+	if connection, err := sql.Open("mysql", url); err != nil {
+		panic(err)
+	} else {
+		db = connection
+		fmt.Println("Conexion ok")
+	}
+}
+
+//Para la DB en general:
+
+func Ping() {
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+}
+
+func CloseConnection() {
+	db.Close()
+}
+
+//Para las tablas en especifico:
+
+func existsTable(tableName string) bool {
+	sql := fmt.Sprintf("SHOW TABLES LIKE '%s'", tableName)
+	rows, _ := Query(sql)
+	return rows.Next()
+}
+
+func CreateTables() {
+	createTable("users", userSchema)
+}
+
+func createTable(tableName, schema string) {
+	if !existsTable(tableName) {
+		Exec(schema)
+	} else {
+		//truncateTable(tableName)
+	}
+}
+
+func truncateTable(tableName string) {
+	sql := fmt.Sprintf("TRUNCATE %s", tableName)
+	Exec(sql)
+}
+
+//Funciones ejecución de sentencias encapsuladas:
+
+func Exec(query string, args ...interface{}) (sql.Result, error) {
+	result, err := db.Exec(query, args...)
+	if err != nil {
+		log.Println(err)
+	}
+	return result, err
+}
+
+func Query(query string, args ...interface{}) (*sql.Rows, error) {
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		log.Println(err)
+	}
+	return rows, err
+}
