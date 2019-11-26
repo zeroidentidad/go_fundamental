0958356cbbf458df03e4500584692d1cddf93177
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-26 11:11:36 -0600

    go for web
    base64 encoding use

---
+++ ejercicios_go_for_webdev/24_webdev_toolkit/2_base64/base/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	s := "El amor no es más que una canción para cantar. El miedo es la forma en que morimos. Puedes hacer sonar las montañas. O hacer llorar a los ángeles. Aunque el pájaro está en el ala. Y es posible que no sepas por qué. Vamos gente ahora. Sonríe a tu hermano. Todos se juntan. Intenta amar uno al otro ahora."
+
+	encodeStd := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	s64 := base64.NewEncoding(encodeStd).EncodeToString([]byte(s))
+
+	fmt.Println("\nLong original:", len(s))
+	fmt.Println("\nLong codificada:", len(s64))
+	fmt.Println("\nOriginal:", s)
+	fmt.Println("\nCodificada:", s64)
+}
