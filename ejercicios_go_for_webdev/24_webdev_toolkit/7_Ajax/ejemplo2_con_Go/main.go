420400c3c1b5466067561b6e913d5143a816ba92
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-11-26 20:32:52 -0600

    go for web
    ajax use

---
+++ ejercicios_go_for_webdev/24_webdev_toolkit/7_Ajax/ejemplo2_con_Go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/watahea", watahea)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func watahea(w http.ResponseWriter, r *http.Request) {
+	s := `Aquí hay texto de watahea`
+	fmt.Fprintln(w, s)
+}
