d8dee1af16e7f79e92c15adb268b8d3a81131a8d
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-01 16:19:35 -0600

    web basics
    simple end to end test

---
+++ ejercicios_go_web_basics/6_unit_testing/end_to_end/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/urfave/negroni"
+)
+
+func HelloWorld(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	fmt.Fprint(res, "Hello World")
+}
+func App() http.Handler {
+	n := negroni.Classic()
+	m := func(res http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+		fmt.Fprint(res, "Before...")
+		next(res, req)
+		fmt.Fprint(res, "...After")
+	}
+	n.Use(negroni.HandlerFunc(m))
+	r := httprouter.New()
+	r.GET("/", HelloWorld)
+	n.UseHandler(r)
+	return n
+}
+func main() {
+	http.ListenAndServe(":8080", App())
+}
