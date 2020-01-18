501e4ecba896dca3e863384364617c1b8068820a
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-01-18 12:59:25 -0600

    ej golang-es
    chanel con gorutina simple

---
+++ ejercicios_lenguaje_go-es/16_channels_comunicar_gorutina/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal1 := make(chan time.Duration)
+	go bucle(canal1)
+	fmt.Println("Llegué hasta aquí")
+
+	msg := <-canal1
+	fmt.Println(msg)
+}
+
+func bucle(canal1 chan time.Duration) {
+	inicio := time.Now()
+	for i := 0; i < 10000000000; i++ {
+
+	}
+	final := time.Now()
+	canal1 <- final.Sub(inicio)
+}
