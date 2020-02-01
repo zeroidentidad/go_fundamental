3c8b1b50290382c3111ff17db81b3e253d12a55a
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-02-01 17:57:07 -0600

    machine learning
    metricas continuas

--- Xmachine_learning_go/3_evaluacion-validacion/1_evaluacion/1_metricas_continuas/ejemplo2.go
+++ Xmachine_learning_go/3_evaluacion-validacion/1_evaluacion/1_metricas_continuas/ejemplo2.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"math"
 	"os"
 	"strconv"
+
+	"gonum.org/v1/gonum/stat"
 )
 
 func main() {
 
-	// Error Cuadrático Medio (MSE) y Error Absoluto Medio (MAE)
+	// R-Cuadratica ó, coeficiente de determinación
 
 	// Abrir los datos de las observaciones y predicciones continuas.
 	f, err := os.Open("./data/continuous_data.csv")
@@ -24,7 +25,7 @@ func main() {
 	// Crear un nuevo lector CSV que lea del archivo abierto.
 	reader := csv.NewReader(f)
 
-	// observado y predicho contendrá los valores analizados y pronosticados analizados del archivo de datos continuo.
+	// observed y predicted contendrá los valores analizados y pronosticados analizados del archivo de datos continuo.
 	var observed []float64
 	var predicted []float64
 
@@ -34,7 +35,7 @@ func main() {
 	// Leer en los registros buscando tipos inesperados en las columnas.
 	for {
 
-		// Leer en una fila. Comprueba si estamos al final del archivo.
+		// Leer en una fila. Comprobar si estamos al final del archivo.
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
@@ -65,16 +66,9 @@ func main() {
 		line++
 	}
 
-	// Calcular el error absoluto medio y el error cuadrático medio.
-	var mAE float64
-	var mSE float64
-	for idx, oVal := range observed {
-		mAE += math.Abs(oVal-predicted[idx]) / float64(len(observed))
-		mSE += math.Pow(oVal-predicted[idx], 2) / float64(len(observed))
-	}
+	// Calcular el valor R^2.
+	rSquared := stat.RSquaredFrom(observed, predicted, nil)
 
-	// Salida del valor MAE y MSE a la salida estándar.
-	fmt.Printf("\nMAE = %0.2f\n", mAE)
-	fmt.Printf("\nMSE = %0.2f\n\n", mSE)
-
+	// Salida del valor R^2 a la salida estándar.
+	fmt.Printf("\nR^2 = %0.2f\n\n", rSquared)
 }
