15ad609bc2c5c692a0ff32d7bad17d87916135b0
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-04-07 14:18:07 -0500

    recetas estandar lib
    lado del servidor

---
+++ Xej_recetas_estandar_lib_go/9_Lado_del_servidor/receta12_servir_contenido_seguro/servetls.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type SimpleHTTP struct{}
+
+func (s SimpleHTTP) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Hello world")
+}
+
+func main() {
+	fmt.Println("Starting HTTP server on port 8080")
+	// http.ListenAndServe(":8080", SimpleHTTP{})
+	s := &http.Server{Addr: ":8080", Handler: SimpleHTTP{}}
+	if err := s.ListenAndServeTLS("server.crt", "server.key"); err != nil {
+		panic(err)
+	}
+}
