661b1b81d0fb1e0c1ef86d4b60222acac976c703
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-04-10 23:00:59 -0500

    algoritmos ordenacion
    selection sort

---
+++ ej_ordenamientos/2_Selection_Sort/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr_original := []int{412, 12, 123, 1, 2324, 3, 155, 23412, 2}
+
+	arr_ordenado := selection_sort(arr_original)
+
+	fmt.Println(arr_ordenado)
+}
+
+func selection_sort(arreglo []int) []int {
+	for i := 0; i < len(arreglo); i++ {
+		minimo_encontrado, posicion_minimo := arreglo[i], i
+
+		valor_original := arreglo[i]
+		// encontrar minimo en parte desordenada
+		for j := i + 1; j < len(arreglo); j++ {
+			valor_comparacion := arreglo[j]
+			if valor_comparacion < minimo_encontrado {
+				minimo_encontrado, posicion_minimo = valor_comparacion, j
+			}
+		}
+
+		if minimo_encontrado != valor_original {
+			// intercambio posiciones con primer desordenado
+			arreglo[i], arreglo[posicion_minimo] = minimo_encontrado, valor_original
+		}
+	}
+
+	return arreglo
+}
