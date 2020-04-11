15bd762336c24cf09637fd7899dd4ddd806e39f6
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-04-10 21:55:05 -0500

    algoritmos ordenacion
    implementacion insertion sort

---
+++ ej_ordenamientos/1_Insertion _Sort/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr_original := []int{412, 12, 123, 1, 2324, 3, 155, 23412, 2}
+
+	arr_ordenado := insertion_sort(arr_original)
+
+	fmt.Println("\nResultado:")
+	fmt.Println(arr_ordenado)
+}
+
+func insertion_sort(arreglo []int) []int {
+	for i := 1; i < len(arreglo); i++ {
+		j := i
+		for j > 0 && arreglo[j-1] > arreglo[j] {
+			fmt.Printf("%d intercambio -> %d\n", arreglo[j-1], arreglo[j])
+			swap(j-1, j, &arreglo)
+			j--
+		}
+	}
+	return arreglo
+}
+
+func swap(previo, actual int, puntero_arreglo *[]int) {
+	arreglo := *puntero_arreglo
+	copia := arreglo[actual]
+	arreglo[actual] = arreglo[previo]
+	arreglo[previo] = copia
+}
