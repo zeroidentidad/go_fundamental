43e60be94391f3ce24be98d4cb1e4756f741ad4a
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-05-16 19:53:08 -0500

    restwebapp
    integracion ngrx

--- ej_rest_webapp_ventas/frontend/src/app/app.module.ts
+++ ej_rest_webapp_ventas/frontend/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
+import { metaReducers } from "./state/reducers";
 
 @NgModule({
   declarations: [
@@ -12,6 +17,9 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({}, {metaReducers}),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument():[],
     AppRoutingModule,
     SharedModule,
     HomeModule
