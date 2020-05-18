c347c2d488231f851d9617172ed918375bc7c3bb
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-05-18 11:00:48 -0500

    restwebapp
    materialtabs front init

--- ej_rest_webapp_ventas/frontend/src/app/product/product.module.ts
+++ ej_rest_webapp_ventas/frontend/src/app/product/product.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductMainContainerComponent } from './containers/product-main-container/product-main-container.component';
 import { ProductListContainerComponent } from './containers/product-list-container/product-list-container.component';
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
   declarations: [ProductMainContainerComponent, ProductListContainerComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    SharedModule
   ]
 })
 export class ProductModule { }
