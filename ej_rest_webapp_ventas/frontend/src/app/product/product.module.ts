846c3f8329e0c00ed50180c592c191ac3253cccc
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2020-05-17 22:19:31 -0500

    restwebapp
    list container int

--- ej_rest_webapp_ventas/frontend/src/app/product/product.module.ts
+++ ej_rest_webapp_ventas/frontend/src/app/product/product.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductMainContainerComponent } from './containers/product-main-container/product-main-container.component';
+import { ProductListContainerComponent } from './containers/product-list-container/product-list-container.component';
 
 
 @NgModule({
-  declarations: [ProductMainContainerComponent],
+  declarations: [ProductMainContainerComponent, ProductListContainerComponent],
   imports: [
     CommonModule,
     ProductRoutingModule
