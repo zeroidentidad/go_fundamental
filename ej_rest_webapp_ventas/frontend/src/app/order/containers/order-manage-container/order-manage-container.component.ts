9214aefa7ab02963b3a6420b2c80d59e35d0a5f3
zeroidentidad <jafs-2050@hotmail.com>
2020-05-30 23:04:59 -0500

    restwebapp
    vista container manejo orden

--- ej_rest_webapp_ventas/frontend/src/app/order/containers/order-manage-container/order-manage-container.component.ts
+++ ej_rest_webapp_ventas/frontend/src/app/order/containers/order-manage-container/order-manage-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-order-manage-container',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderManageContainerComponent implements OnInit {
 
-  constructor() { }
+  orderForm: FormGroup;
+  constructor(private fb: FormBuilder) { 
+    this.buildNewForm();
+  }
 
   ngOnInit() {
+  }
+
+  buildNewForm() {
+    this.orderForm=this.fb.group({
+      id: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      company: ['', [Validators.required]],
+      fecha: [new Date(), [Validators.required]],
+      address: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      city: ['', [Validators.required]]
+    });
+  }
+
+  openCustomerPopup(){
+    
   }
 
 }
