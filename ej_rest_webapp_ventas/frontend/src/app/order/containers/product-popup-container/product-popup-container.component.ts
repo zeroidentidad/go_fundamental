a93e69c70cd768f39027cf9b6d2d17dcf1897a57
zeroidentidad <jafs-2050@hotmail.com>
2020-06-01 12:38:50 -0500

    restwebapp
    load product data en container

--- ej_rest_webapp_ventas/frontend/src/app/order/containers/product-popup-container/product-popup-container.component.ts
+++ ej_rest_webapp_ventas/frontend/src/app/order/containers/product-popup-container/product-popup-container.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import * as productActions from '../../state/actions/product.actions';
+import * as fromReducer from '../../state/reducers';
+import {Store} from '@ngrx/store';
+import {GetProducts} from "../../models/product/get-product";
+import {Observable} from "rxjs";
+import {Product} from "../../models/product/product";
 
 @Component({
   selector: 'app-product-popup-container',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPopupContainerComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<Product[]>=this.store.select(fromReducer.getProducts);
+  length$: Observable<number>=this.store.select(fromReducer.gettotalProductRecords);
+
+  pageSize=5;
+  pageSizeOptions: number[]=[5, 10, 20, 30];
+  request: GetProducts;
+  constructor(private store: Store<fromReducer.OrderState>) { }
 
   ngOnInit() {
+    this.request=new GetProducts(this.pageSizeOptions[0], 0);
+    this.store.dispatch(new productActions.LoadProducts(this.request));
   }
+
+  changePage(event: any): void {
+    const offSet=event.pageIndex*event.pageSize;
+    this.pageSize=event.pageSize;
+    this.request=new GetProducts(event.pageSize, offSet);
+    this.store.dispatch(new productActions.LoadProducts(this.request));
+  }  
 
 }
