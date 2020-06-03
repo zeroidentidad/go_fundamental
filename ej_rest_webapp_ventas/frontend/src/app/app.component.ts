192428133af8cce84d4933bab76c3e950f989996
zeroidentidad <jafs-2050@hotmail.com>
2020-06-03 18:15:03 -0500

    restwebapp
    init forma autenticacion con Auth0

--- ej_rest_webapp_ventas/frontend/src/app/app.component.ts
+++ ej_rest_webapp_ventas/frontend/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webng';
+
+  isAuthenticated=true;
+
+  login() {
+  }
+
+  logout() {
+  }  
 }
--- ej_rest_webapp_ventas/frontend/src/app/shared/nav-bar/nav-bar.component.ts
+++ ej_rest_webapp_ventas/frontend/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  @Input()
+  isAuthenticated: boolean;
+  @Output()
+  login: EventEmitter<any>=new EventEmitter<any>();
+  @Output()
+  logout: EventEmitter<any>=new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+  }
+
+  onLogout() {
   }
 
 }
