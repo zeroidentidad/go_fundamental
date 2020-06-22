c999bc824c43c721ccfa7059a0eb167617d988b2
zeroidentidad <jafs-2050@hotmail.com>
2020-06-22 17:26:40 -0500

    twback func db modprofile

---
+++ ej_rest_webapp_social/backend/db/modifyRegistry.go
@@ -0,0 +1,57 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/zeroidentidad/twittor/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*ModifyRegistry permite modificar el perfil del usuario */
+func ModifyRegistry(user models.User, ID string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoConn.Database("twittor")
+	col := db.Collection("usuarios")
+
+	registro := make(map[string]interface{})
+	if len(user.Nombre) > 0 {
+		registro["nombre"] = user.Nombre
+	}
+	if len(user.Apellidos) > 0 {
+		registro["apellidos"] = user.Apellidos
+	}
+	registro["fechaNacimiento"] = user.FechaNacimiento
+	if len(user.Avatar) > 0 {
+		registro["avatar"] = user.Avatar
+	}
+	if len(user.Banner) > 0 {
+		registro["banner"] = user.Banner
+	}
+	if len(user.Biografia) > 0 {
+		registro["biografia"] = user.Biografia
+	}
+	if len(user.Ubicacion) > 0 {
+		registro["ubicacion"] = user.Ubicacion
+	}
+	if len(user.SitioWeb) > 0 {
+		registro["sitioWeb"] = user.SitioWeb
+	}
+
+	updtString := bson.M{
+		"$set": registro,
+	}
+
+	objID, _ := primitive.ObjectIDFromHex(ID)
+	filtro := bson.M{"_id": bson.M{"$eq": objID}}
+
+	_, err := col.UpdateOne(ctx, filtro, updtString)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
