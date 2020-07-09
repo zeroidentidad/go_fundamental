a03e7e4a5e8aa947babb529608b5e79578797d4e
zeroidentidad <jafs-2050@hotmail.com>
2020-07-08 20:39:22 -0500

    backtw handler relacionar users

--- ej_rest_webapp_social/backend/handlers/handlers.go
+++ ej_rest_webapp_social/backend/handlers/handlers.go
@@ -19,10 +19,12 @@ var portString = fmt.Sprintf("%s", confObj.HttpPort)
 func Handlers() {
 	router := mux.NewRouter()
 
+	// User crud data
 	router.HandleFunc("/registro", middlewares.CheckConnectionDB(routes.Registry)).Methods(http.MethodPost)
 	router.HandleFunc("/login", middlewares.CheckConnectionDB(routes.Login)).Methods(http.MethodPost)
 	router.HandleFunc("/verperfil", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.ViewProfile))).Methods(http.MethodGet)
 	router.HandleFunc("/modificarperfil", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.ModifyProfile))).Methods(http.MethodPut)
+	// Tweets crud data
 	router.HandleFunc("/tweet", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.SendTweet))).Methods(http.MethodPost)
 	router.HandleFunc("/vertweets", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.ViewTweets))).Methods(http.MethodGet)
 	router.HandleFunc("/eliminartweet", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.RemoveTweet))).Methods(http.MethodDelete)
@@ -32,6 +34,8 @@ func Handlers() {
 	// Banners image
 	router.HandleFunc("/subirbanner", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.UploadBanner))).Methods(http.MethodPost)
 	router.HandleFunc("/obtenerbanner", middlewares.CheckConnectionDB(routes.FindBanner)).Methods(http.MethodGet)
+	// Relaciones users
+	router.HandleFunc("/relacionar", middlewares.CheckConnectionDB(middlewares.ValidateJWT(routes.SetRelation))).Methods(http.MethodPost)
 
 	PORT := os.Getenv("PORT")
 
