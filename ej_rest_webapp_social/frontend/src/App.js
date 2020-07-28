b1bf593029d9d4f2daa64671dcbae2fe937b3e7e
zeroidentidad <jafs-2050@hotmail.com>
2020-07-27 19:07:55 -0500

    frontw def init login

--- ej_rest_webapp_social/frontend/src/App.js
+++ ej_rest_webapp_social/frontend/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [user, setUser]=useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user? (<h1>Logueado</h1>):(<h1>No logueado</h1>)}
     </div>
-  );
+  )
+
 }
 
 export default App;
