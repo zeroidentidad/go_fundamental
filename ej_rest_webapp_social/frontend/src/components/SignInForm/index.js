092115fce4d39b4fedcde0d6ed825e937fa637ba
zeroidentidad <jafs-2050@hotmail.com>
2020-08-15 13:16:28 -0500

    frontw signup form login context token

---
+++ ej_rest_webapp_social/frontend/src/components/SignInForm/index.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Form, Button, Spinner } from "react-bootstrap";
+import { values, size } from "lodash";
+import { toast } from "react-toastify";
+import { isEmailValid } from "../../utils/validations";
+import { signInApi, setTokenApi } from "../../api/auth";
+
+import "./SignInForm.scss";
+
+export default function SignInForm(props) {
+    const {setRefreshCheckLogin}=props;
+    const [formData, setFormData]=useState(initialFormValue());
+    const [signInLoading, setSignInLoading]=useState(false);
+
+    const onSubmit=e => {
+        e.preventDefault();
+
+        let validCount=0;
+        values(formData).some(value => {
+            value&&validCount++;
+            return null;
+        });
+
+        if (size(formData)!==validCount) {
+            toast.warning("Completa los campos del formulario");
+        } else {
+            if (!isEmailValid(formData.email)) {
+                toast.warning("Email es invalido");
+            } else {
+                setSignInLoading(true);
+                signInApi(formData)
+                    .then(response => {
+                        if (response.message) {
+                            toast.warning(response.message);
+                        } else {
+                            setTokenApi(response.token);
+                            setRefreshCheckLogin(true);
+                        }
+                    })
+                    .catch(() => {
+                        toast.error("Error de servidor, intentar más tarde!");
+                    })
+                    .finally(() => {
+                        setSignInLoading(false);
+                    });
+            }
+        }
+    };
+
+    const onChange=e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <div className="sign-in-form">
+            <h2>Ingresar</h2>
+            <Form onSubmit={onSubmit} onChange={onChange}>
+                <Form.Group>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        placeholder="Correo electronico"
+                        defaultValue={formData.email}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        placeholder="Contraseña"
+                        defaultValue={formData.password}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    {!signInLoading? "Iniciar sesión":<Spinner animation="border" />}
+                </Button>
+            </Form>
+        </div>
+    );
+}
+
+const initialFormValue = () => ({
+    email: "",
+    password: ""
+});
