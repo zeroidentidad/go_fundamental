ebc11fcd9e3c594df420ab8bbaa5f2f901357e76
zeroidentidad <jafs-2050@hotmail.com>
2020-08-22 14:04:36 -0500

    frontw base layout init

--- ej_rest_webapp_social/frontend/src/pages/Home/index.js
+++ ej_rest_webapp_social/frontend/src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Home.scss";
+import {BasicLayout} from "../../layout"
 
 export default function Home() {
     return (
-        <div>
-            <h1>En home</h1>
-        </div>
+    <BasicLayout>
+        <h1>En home</h1>
+    </BasicLayout>
     )
 }
