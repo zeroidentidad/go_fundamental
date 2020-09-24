1276b283d52dc246339af39b66bbbc282dd4cb61
zeroidentidad <jafs-2050@hotmail.com>
2020-09-23 20:21:07 -0500

    frontw obtener datos usuario de la api

--- ej_rest_webapp_social/frontend/src/pages/User/index.js
+++ ej_rest_webapp_social/frontend/src/pages/User/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Spinner} from "react-bootstrap";
+import {withRouter} from "react-router-dom";
 import {BasicLayout} from "../../layout";
+import {getUserApi} from "../../api/user";
+import { toast } from "react-toastify";
 
 import "./User.scss";
 
-export default function User() {
+function User(props) {
+    const {match}=props;
+    const {params}=match;
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+
+        getUserApi(params.id)
+        .then(res => {
+            if (!res) toast.error("El usuario no existe");
+            setUser(res);
+        })
+        .catch(() => {
+            toast.error("El usuario no existe");
+        });
+
+    }, [params])
+
     return (
         <BasicLayout className="user">
             <div className="user__title">
@@ -16,3 +36,5 @@ export default function User() {
         </BasicLayout>
     )
 }
+
+export default withRouter(User)
