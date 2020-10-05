39882fdf9e1f9d3e8d4538c6a935761be42aec6d
zeroidentidad <jafs-2050@hotmail.com>
2020-10-05 16:31:26 -0500

    snippetweb basic routing

--- ej_rest_webapp_snippets/main.go
+++ ej_rest_webapp_snippets/main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	const port = "4000"
 	log.Println("Server start on port:", port)
@@ -16,5 +18,22 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Home Snippets"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("New snippet..."))
 }
