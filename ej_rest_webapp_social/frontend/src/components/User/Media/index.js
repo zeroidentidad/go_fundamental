249557cff498feadf90e2fca82d720046618a81a
zeroidentidad <jafs-2050@hotmail.com>
2020-10-13 00:36:50 -0500

    frontw mostrar avatar

--- ej_rest_webapp_social/frontend/src/components/User/Media/index.js
+++ ej_rest_webapp_social/frontend/src/components/User/Media/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { API_HOST } from "../../../utils/config";
-
+import AvatarNotFound from "../../../assets/png/avatar-no-found.png";
 import "./Media.scss";
 
 export default function Media(props) {
     const {user}=props;
     const bannerUrl=user?.banner? `${API_HOST}/obtenerbanner?id=${user.id}`:null;
+    const avatarUrl=user?.avatar? `${API_HOST}/obteneravatar?id=${user.id}`:AvatarNotFound;
     return (
         <div className="banner-avatar" style={{ backgroundImage: `url('${bannerUrl}')` }}>
-            <div className="avatar"></div>
+            <div className="avatar" style={{ backgroundImage: `url('${avatarUrl}')` }}></div>
         </div>
     )
 }
