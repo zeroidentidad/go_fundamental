4b928e7afcc25e656fc2c5cedd9d8c0380e96cab
zeroidentidad <jafs-2050@hotmail.com>
2020-12-17 23:43:28 -0600

    frontw actualizar datos en formedit, banner, avatar

--- ej_rest_webapp_social/frontend/src/components/User/EditUserForm/index.js
+++ ej_rest_webapp_social/frontend/src/components/User/EditUserForm/index.js
@@ -1,7 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import { Form, Button, Row, Col, Spinner } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import es from "date-fns/locale/es";
+import { useDropzone } from "react-dropzone";
+import { API_HOST } from "../../../utils/config";
+import { Camera } from "../../../utils/icons";
 
 import "./EditUserForm.scss";
 
@@ -9,6 +12,50 @@ export default function EditUserForm(props) {
     const { user, setShowModal } = props;
     const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState(initialFormValue(user));
+
+    const [bannerUrl, setBannerUrl] = useState(
+        user?.banner ? `${API_HOST}/obtenerbanner?id=${user.id}` : null
+    );
+    const [bannerFile, setBannerFile] = useState(null);
+    
+    const [avatarUrl, setAvatarUrl] = useState(
+        user?.avatar ? `${API_HOST}/obteneravatar?id=${user.id}` : null
+    );
+    const [avatarFile, setAvatarFile] = useState(null);    
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const onDropBanner = useCallback((acceptedFile) => {
+        const file = acceptedFile[0];
+        setBannerUrl(URL.createObjectURL(file));
+        setBannerFile(file);   
+    });
+
+    const {
+        getRootProps: getRootBannerProps,
+        getInputProps: getInputBannerProps,
+    } = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        multiple: false,
+        onDrop: onDropBanner,
+    });
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const onDropAvatar = useCallback((acceptedFile) => {
+        const file = acceptedFile[0];
+        setAvatarUrl(URL.createObjectURL(file));
+        setAvatarFile(file);
+    });
+
+    const {
+        getRootProps: getRootAvatarProps,
+        getInputProps: getInputAvatarProps,
+    } = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        multiple: false,
+        onDrop: onDropAvatar,
+    });    
     
     const onChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -21,6 +68,22 @@ export default function EditUserForm(props) {
 
     return (
         <div className="edit-user-form">
+            <div className="banner"
+            style={{ backgroundImage: `url('${bannerUrl}')` }}
+            {...getRootBannerProps()}
+            >
+                <input {...getInputBannerProps()} />
+                <Camera />
+            </div>
+            
+            <div className="avatar"
+            style={{ backgroundImage: `url('${avatarUrl}')` }}
+            {...getRootAvatarProps()}
+            >
+                <input {...getInputAvatarProps()} />
+                <Camera />
+            </div>
+
             <Form onSubmit={onSubmit}>
                 <Form.Group>
                 <Row>
