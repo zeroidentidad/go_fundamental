af3f759f9f504d4c8a35350d33d4f4026cec87e7
zeroidentidad <jafs-2050@hotmail.com>
2020-12-20 17:32:10 -0600

    cargar mas

--- ej_rest_webapp_social/frontend/src/pages/User/index.js
+++ ej_rest_webapp_social/frontend/src/pages/User/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {withRouter} from "react-router-dom";
 import { toast } from "react-toastify";
+import { Button, Spinner } from "react-bootstrap";
 import {BasicLayout} from "../../layout";
 import Media from "../../components/User/Media";
 import InfoUser from "../../components/User/InfoUser";
@@ -16,6 +17,8 @@ function User(props) {
     const {params}=match;
     const [user, setUser] = useState(null);
     const [tweets, setTweets] = useState(null);
+    const [page, setPage] = useState(1);
+    const [loadingTweets, setLoadingTweets] = useState(false);
     const loggedUser=useAuth();
 
     useEffect(() => {
@@ -37,7 +40,22 @@ function User(props) {
         .catch(() => {
             setTweets([]);
         });
-    }, [params]);    
+    }, [params]);
+    
+    const moreData = () => {
+        const pageTemp = page + 1;
+        setLoadingTweets(true);
+
+        getUserTweetsApi(params.id, pageTemp).then((response) => {
+        if (!response) {
+            setLoadingTweets(0);
+        } else {
+            setTweets([...tweets, ...response]);
+            setPage(pageTemp);
+            setLoadingTweets(false);
+        }
+        });
+    };    
 
     return (
         <BasicLayout className="user" setRefreshCheckLogin={setRefreshCheckLogin}>
@@ -49,6 +67,19 @@ function User(props) {
             <div className="user__tweets">
                 <h3>Tweets</h3>
                 {tweets && <ListTweets tweets={tweets} />}
+                <Button onClick={moreData}>
+                {!loadingTweets ? (
+                    loadingTweets !== 0 && "Obtener más Tuits"
+                ) : (
+                    <Spinner
+                    as="span"
+                    animation="grow"
+                    size="sm"
+                    role="status"
+                    arian-hidden="true"
+                    />
+                )}
+                </Button>                
             </div>
         </BasicLayout>
     )
