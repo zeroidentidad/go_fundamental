2c1703b6179ea47ff88e29d8c4c3c3f9a12c9ef4
zeroidentidad <jafs-2050@hotmail.com>
2021-02-13 00:44:34 -0600

    fiber train

--- ej_fiber-hex-apidoc/app/start.go
+++ ej_fiber-hex-apidoc/app/start.go
@@ -1,13 +1,19 @@
 package app
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	"github.com/zeroidentidad/fiber-hex-apidoc/domain"
+	logs "github.com/zeroidentidad/fiber-hex-apidoc/logger"
 	"github.com/zeroidentidad/fiber-hex-apidoc/service"
 )
 
 func Start() {
+	sanityCheck()
 	router := fiber.New()
 	router.Use(logger.New())
 
@@ -17,5 +23,32 @@ func Start() {
 	router.Get("/clientes", hc.getAllClientes)
 	router.Get("/clientes/:id", hc.getCliente)
 
-	_ = router.Listen(":3000")
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+	logs.Info(fmt.Sprintf("Starting server on %s:%s ...", address, port))
+	err := router.Listen(fmt.Sprintf("%s:%s", address, port))
+	logs.Fatal(err.Error())
+
+}
+
+func sanityCheck() {
+	err := godotenv.Load()
+	if err != nil {
+		logs.Fatal("Error loading .env file")
+	}
+
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWD",
+		"DB_ADDR",
+		"DB_PORT",
+		"DB_NAME",
+	}
+	for _, k := range envProps {
+		if os.Getenv(k) == "" {
+			logs.Fatal(fmt.Sprintf("Env variable %s not defined. Terminating app...", k))
+		}
+	}
 }
--- ej_fiber-hex-apidoc/go.mod
+++ ej_fiber-hex-apidoc/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gofiber/fiber/v2 v2.3.3
 	github.com/jmoiron/sqlx v1.3.1
+	github.com/joho/godotenv v1.3.0
 	go.uber.org/zap v1.16.0
 )
