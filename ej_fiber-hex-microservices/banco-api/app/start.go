814a1a2a8d270f38c4d89779b5438a7705ba271b
zeroidentidad <jafs-2050@hotmail.com>
2021-02-14 20:30:32 -0600

    fiber train

--- ej_fiber-hex-microservices/banco-api/app/start.go
+++ ej_fiber-hex-microservices/banco-api/app/start.go
@@ -3,12 +3,9 @@ package app
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
 	"github.com/zeroidentidad/fiber-hex-api/domain"
 	logs "github.com/zeroidentidad/fiber-hex-api/logger"
 	"github.com/zeroidentidad/fiber-hex-api/service"
@@ -36,46 +33,4 @@ func Start() {
 	logs.Info(fmt.Sprintf("Starting server on %s:%s ...", address, port))
 	err := router.Listen(fmt.Sprintf("%s:%s", address, port))
 	logs.Fatal(err.Error())
-}
-
-func sanityCheck() {
-	err := godotenv.Load()
-	if err != nil {
-		logs.Fatal("Error loading .env file")
-	}
-
-	envProps := []string{
-		"SERVER_ADDRESS",
-		"SERVER_PORT",
-		"DB_USER",
-		"DB_PASSWD",
-		"DB_ADDR",
-		"DB_PORT",
-		"DB_NAME",
-	}
-	for _, k := range envProps {
-		if os.Getenv(k) == "" {
-			logs.Fatal(fmt.Sprintf("- %s not defined. Terminating app...", k))
-		}
-	}
-}
-
-func getDbClient() *sqlx.DB {
-	dbUser := os.Getenv("DB_USER")
-	dbPasswd := os.Getenv("DB_PASSWD")
-	dbAddr := os.Getenv("DB_ADDR")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
-	client, err := sqlx.Open("mysql", dataSource)
-	if err != nil {
-		panic(err)
-	}
-
-	//pool settings:
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
-	return client
 }
