10389ca68f865bf4e8245c6132427dcd3407b437
zeroidentidad <jafs-2050@hotmail.com>
2021-03-18 12:52:24 -0600

    pastein avance

--- ej_rest_webapp_snippets/cmd/web/templates.go
+++ ej_rest_webapp_snippets/cmd/web/templates.go
@@ -4,11 +4,21 @@ import (
 	"pastein/pkg/models"
 	"path/filepath"
 	"text/template"
+	"time"
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -26,7 +36,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		// Extraer nombre del archivo (como 'home.page.tmpl') de ruta completa del archivo
 		// y asignar a la variable de name.
 		name := filepath.Base(page)
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
