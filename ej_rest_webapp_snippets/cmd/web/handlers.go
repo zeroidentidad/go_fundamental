4f19a73c5398fc8d6f94fe0ede6cb32c3986101c
zeroidentidad <jafs-2050@hotmail.com>
2021-03-24 10:09:57 -0600

    pastein avance

--- ej_rest_webapp_snippets/cmd/web/handlers.go
+++ ej_rest_webapp_snippets/cmd/web/handlers.go
@@ -118,7 +118,7 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display user login form...")
+	app.render(w, r, "login.page.tmpl", &templateData{Form: forms.New(nil)})
 }
 
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
