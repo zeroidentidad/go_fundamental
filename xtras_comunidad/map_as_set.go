e78349d4e9a3250414a0042d7026b474bc1654f9
zeroidentidad <jafs-2050@hotmail.com>
2021-05-27 22:07:51 -0500

    map as set

---
+++ xtras_comunidad/map_as_set.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	intSet := map[int]bool{}
+	vals := []int{5, 10, 2, 5, 8, 7, 3, 9, 1, 2, 10}
+	for _, v := range vals {
+		intSet[v] = true
+	}
+
+	fmt.Println(len(vals), len(intSet))
+	fmt.Println(intSet[5])
+	fmt.Println(intSet[500])
+	if intSet[100] {
+		fmt.Println("100 is in the set")
+	}
+
+	withStruct()
+}
+
+func withStruct() {
+	intSet := map[int]struct{}{}
+	vals := []int{5, 10, 2, 5, 8, 7, 3, 9, 1, 2, 10}
+	for _, v := range vals {
+		intSet[v] = struct{}{}
+	}
+
+	if _, ok := intSet[5]; ok {
+		fmt.Println("5 in set")
+	}
+}
